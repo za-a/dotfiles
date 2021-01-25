2a00a8d753aa1753d77c87aa04a80b2b62346b06
za-a <a.3axapo8@ya.ru>
2021-01-25 23:00:00 +0300

    New backup 2021-01-25 23:00:00

--- backup.sh
+++ backup.sh
@@ -8,8 +8,8 @@ else
   exit 1
 fi
 
-cp "$HOME/{.zshrc,.vimrc,check.sh}"
+cp "$HOME/{.zshrc,.vimrc,check.sh} ."
 
-git add -u 
+git add -u
 git commit -m "New backup `date +'%Y-%m-%d %H:%M:%S'`";
 git push
