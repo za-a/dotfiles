bba4e76322c3c4b20872eff0cc82e4b8fdba9ad2
za-a <a.3axapo8@ya.ru>
2021-01-25 22:07:45 +0300

    Updated backup script

--- backup.sh
+++ backup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+IS_GIT_AVAILABLE="$(git --version)"
+if [[ $IS_GIT_AVAILABLE == *"version"* ]]; then
+  echo "Git is Available"
+else
+  echo "Git is not installed"
+  exit 1
+fi
+
 cp $HOME/{.zshrc,.vimrc} .
